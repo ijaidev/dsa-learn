fb7a10e944135fe12170e10dd2283d01f32f0f19
Jaidev Singh <jaddybanna@gmail.com>
2026-03-24 10:56:07 +0000

    init

---
+++ ch4/l1/main.py
@@ -0,0 +1,23 @@
+class Influencer:
+    def __init__(self, num_selfies, num_bio_links):
+        self.num_selfies = num_selfies
+        self.num_bio_links = num_bio_links
+
+    def __repr__(self):
+        return f"({self.num_selfies}, {self.num_bio_links})"
+
+
+# dont touch above this line
+
+
+
+def vanity(influencer):
+    return (influencer.num_bio_links * 5) + influencer.num_selfies
+
+def key(inf):
+    return vanity(inf)
+
+def vanity_sort(influencers):
+    infs = influencers
+    return sorted(infs, key=key)
+
