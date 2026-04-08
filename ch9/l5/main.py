7c0af295943808648e5d39005b23db43a1ca10c3
Jaidev Singh <jaddybanna@gmail.com>
2026-04-08 10:03:52 +0000

    ch9

---
+++ ch9/l5/main.py
@@ -0,0 +1,29 @@
+from node import Node
+
+
+class LinkedList:
+    def add_to_tail(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        last = None
+        for item in self:
+            last = item
+        last.set_next(node)
+
+    # don't touch below this line
+
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(node.val)
+        return " -> ".join(nodes)
